e799be30b45d3bca5906170500c6e52fd7075336
NatNichols <natalie.e.nichols@gmail.com>
2024-01-11 19:47:14 -0500

    add app skeleton, import express module, create Express app, link to port 3000

---
+++ server.js
@@ -0,0 +1,24 @@
+// import modules
+import express from 'express'
+
+
+// create Express app
+const app = express()
+
+
+// configure the app (app.set)
+
+
+
+// mount Middleware (app.use)
+
+
+
+// mount routes
+
+
+
+// tell the app to listen on port 3000
+app.listen(3000, function() {
+  console.log(`Listening on port 3000`)
+})
